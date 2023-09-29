0e23f63b395b64cb0e543e107123eda7d368deac
Sourabh Kothari <jainjain390@gmail.com>
2023-09-30 00:17:17 +0530

    update: decreased animation speed

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,22 +38,22 @@ const sr = ScrollReveal({
 /*SCROLL HOME*/
 sr.reveal('.home__title',{}); 
 sr.reveal('.button',{delay: 100}); 
-sr.reveal('.home__img',{delay: 200}); 
-sr.reveal('.home__social-icon',{ interval: 150}); 
+sr.reveal('.home__img',{delay: 100}); 
+sr.reveal('.home__social-icon',{ interval: 50}); 
 
 /*SCROLL ABOUT*/
 sr.reveal('.about__img',{}); 
 sr.reveal('.about__subtitle',{delay: 100}); 
-sr.reveal('.about__text',{delay: 200}); 
+sr.reveal('.about__text',{delay: 50}); 
 
 /*SCROLL SKILLS*/
 sr.reveal('.skills__subtitle',{}); 
 sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 400});
+sr.reveal('.skills__data',{interval: 150}); 
+sr.reveal('.skills__img',{delay: 50});
 
 /*SCROLL WORK*/
-sr.reveal('.work__img',{interval: 100}); 
+sr.reveal('.work__img',{interval: 50}); 
 
 /*SCROLL CONTACT*/
 sr.reveal('.contact__input',{interval: 100}); 
